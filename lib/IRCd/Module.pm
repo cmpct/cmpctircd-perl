e1bc836adf5e936b54e357abee84676dc9ed8c23
sam <sam@cmpct.info>
2016-08-26 20:09:29 +0100

    Support generic events
    * Generic events are events which can be fired for any type of action, such as a user joining a room.
    * Events will be created and documented as time goes on, most likely called from the responsible core IRCd module.

--- lib/IRCd/Module.pm
+++ lib/IRCd/Module.pm
@@ -5,17 +5,23 @@ use Carp;
 use Tie::RefHash;
 package IRCd::Module;
 
+# IRCd::Module provides two methods of 'events':
+# 1) 'command' events, which are their own way of registering received commands;
+# 2) generic events, such as handle_user_pong which provide a way of hooking into
+# more specific behaviour.
 sub new {
     my $class = shift;
     my $self  = {
         'ircd'     => shift,
         'modules'  => {},
         'handlers' => {},
+        'events'   => {},
     };
     bless $self, $class;
     return $self;
 }
 
+# Command codes
 sub register_cmd {
     my $self    = shift;
     my $packet  = uc(shift);
@@ -84,6 +90,49 @@ sub is_loaded_module {
     my $module = shift;
     return 1 if($self->{modules}->{$module});
     return 0;
+}
+
+#              #
+#    Events    #
+#              #
+sub register_event {
+    my $self    = shift;
+    my $event   = lc(shift);
+    my $ref     = shift;
+    my $args    = \@_;
+    # Needed to use refs as a hash key
+    if(!$self->{events}->{$event}) {
+        tie $self->{events}->{$event}->%*, 'Tie::RefHash';
+    }
+    $self->{events}->{$event}->{$ref} = $args;
+}
+sub unregister_event {
+    my $self     = shift;
+    my $event    = lc(shift);
+    my $ref      = shift;
+    delete $self->{events}->{$event}->{$ref};
+}
+
+sub fire_event {
+    my $self     = shift;
+    my $event    = lc(shift);
+    my @handlers = keys($self->{events}->{$event}->%*);
+    my $userArgs = \@_;
+    my $found    = 0;
+    my %returnValues = {};
+    # Execute all registered handlers for $event
+    # Keep a list of all of the return values
+    foreach(@handlers) {
+        my $ref  = $_;
+        my $args = $self->{events}->{$event}->{$_};
+        $returnValues{$ref} = $_->($args, $userArgs);
+        $found = 1;
+    }
+    # returns if we found a handler or not
+    return {
+        'values' => $returnValues,
+        'found'  => $found,
+    };
 }
 
 #             #
